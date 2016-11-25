c5358eb871ecb1ddffa80686e02254b4d0fe8bf4
zhannur-diyas <misyzboston@gmail.com>
2016-11-25 22:43:25 +0600

    Connection pool added

--- src/main/java/cp/ConnectionPool.java
+++ src/main/java/cp/ConnectionPool.java
@@ -6,25 +6,33 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 /**
  * Zhannur Diyas
  * 11/25/2016 | 10:03 PM
  */
+
 public class ConnectionPool {
     private static final Logger log = LoggerFactory.getLogger(ConnectionPool.class);
     private static final String PROPERTY_FILE_NAME = "db.properties";
 
     private static volatile ConnectionPool instance;
-    private BlockingQueue<Connection> connectionQueue;
+    private BlockingQueue<Connection> connections;
     private String driverName;
     private String url;
     private String username;
     private String password;
     private int poolSize;
+
+    private ConnectionPool() {
+        loadProperties();
+        init();
+    }
 
     static ConnectionPool getInstance() {
         return InstanceHolder.instance;
@@ -52,10 +60,24 @@ public class ConnectionPool {
         } catch (IOException e) {
             log.error("Failed to load the property file");
         }
-        driverName = properties.getProperty("h2.driver");
-        url = properties.getProperty("h2.url");
-        username = properties.getProperty("h2.username");
-        password = properties.getProperty("h2.password");
+        this.driverName = properties.getProperty("jdbc.driver");
+        this.url = properties.getProperty("jdbc.url");
+        this.username = properties.getProperty("jdbc.username");
+        this.password = properties.getProperty("jdbc.password");
+        //TODO: invalid poolsize exception handling
+        this.poolSize = Integer.parseInt(properties.getProperty("jdbc.poolsize"));
+    }
+
+    private void init() {
+        connections = new ArrayBlockingQueue<Connection>(poolSize);
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            for (int i = 0; i < poolSize; i++) {
+                connections.add(connection);
+            }
+        } catch (SQLException e) {
+            log.debug("Couldn't load properties. Check the property file.");
+        }
     }
 
     private static class InstanceHolder {
